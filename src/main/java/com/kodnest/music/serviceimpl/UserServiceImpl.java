4ea8e3b9712f5e355f4e9653af847727249aa209
unknown <poojaripavankumar1199@gmail.com>
2024-05-02 07:32:17 +0530

    my musictune project

---
+++ src/main/java/com/kodnest/music/serviceimpl/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.kodnest.music.serviceimpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kodnest.music.entity.User;
+import com.kodnest.music.repository.UserRepository;
+import com.kodnest.music.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+@Autowired	
+UserRepository ur;
+
+@Override
+public void postUser(User user) {
+	ur.save(user);
+	
+}
+public User emailExist(User user) {
+	User details=ur.findByEmail(user.getEmail());
+	if(details==null) {
+		return null;
+	}
+	else return user;
+}
+@Override
+public boolean validUser(String email, String password) {
+	User user=ur.findByEmail(email);
+	
+	
+	if(user==null) {
+		return false;
+	}
+	String dbpwd=user.getPassword();
+	 if(password.equals(dbpwd)&&email.equals(user.getEmail())) {
+			return true;
+		}
+		return false;
+}
+@Override
+public String getRole(String email) {
+     User user=ur.findByEmail(email);
+     return user.getRole();
+}
+
+	
+
+}
